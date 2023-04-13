99fe29ee97dde6269b825fe7168728bb11753d99
Rajdev Yadav <ryindia2023@gmail.com>
2023-04-13 12:10:03 -0400

    Completed Project But Copy Text Button Is Not Working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { useState } from 'react';
+import './App.css';
+import Header from './components/Header';
+
+import kids from './media/kids.png'
 function App() {
+  const[text, setText]=useState('');
+// UpperCase 
+  function handleUpperCase(){
+    let newText = text.toUpperCase();
+    setText(newText)
+  }
+  // LowerCase
+  function handleLowerCase(){
+    let newText = text.toLowerCase();
+    setText(newText);
+  }
+  // Capitalize
+  function handleCapitalize(){
+    // Breaking into words
+    let arr = text.split(" ");
+    // Capitalizing all words 
+    for(let i=0; i< arr.length ; i++){
+      arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+    }
+    // Joining all words
+    let arr2 = arr.join(' ');
+    // Setting Value
+    setText(arr2)
+  }
+  // Copying 
+  const copyToClipboard = text => {
+    if (navigator && navigator.clipboard && navigator.clipboard.writeText)
+      return navigator.clipboard.writeText(text);
+    return Promise.reject('The Clipboard API is not available.');
+  };
+// Remove
+function handleRemove(){
+  setText('')
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <div className="banner">
+      <img src={kids} alt="kids" />
+      </div>
+
+      <div className="main">
+      <div className='sidebar'>
+      <p>Control Panal</p>
+      <button onClick={handleUpperCase}>UpperCase</button>
+      <button onClick={handleLowerCase}>LowerCase</button>
+      <button onClick={handleCapitalize}>Capitalize</button>
+      <button onClick={copyToClipboard}>Copy Text</button>
+      <button onClick={handleRemove}>Remove Text</button>
+    </div>
+
+    <div className='input-field'>
+      <textarea value={text} placeholder='Write Text Here...' onChange={(e)=>setText(e.target.value)}>{text}</textarea>
+    </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <h1>DigiText</h1>
+      <p>By Rajdev Yadav</p>
+      
+    </div>
+  )
+}
+
+export default Header
